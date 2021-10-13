1b54a2a726710db6e4b0b7d22a715c5ea2298417
slerner@splunk.com <slerner@splunk.com>
2021-10-13 19:14:15 -0400

    commit

--- setup-tools/k3s-env-only.sh
+++ setup-tools/k3s-env-only.sh
@@ -3,25 +3,29 @@ sudo apt-get -y update
 #install helm
 curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
+#install k3s
+curl -sfL https://get.k3s.io | sh -
+
 #install otel
 helm repo add splunk-otel-collector-chart https://signalfx.github.io/splunk-otel-collector-chart
 helm repo update
 
 #enable helm to access cluster
-mkdir /home/ubuntu/.kube && sudo kubectl config view --raw > /home/ubuntu/.kube/config
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+sudo chmod 755 /etc/rancher/k3s/k3s.yaml
 
 #install text browser
 sudo apt install -y lynx
 
-#clone workshop
-git clone https://github.com/signalfx/apmworkshop
-
 #install k9s
 curl -sS https://webinstall.dev/k9s | bash
 
+#clone workshop
+git clone https://github.com/signalfx/otelworkshop
+
 #update .bashrc for workshop
-curl https://raw.githubusercontent.com/signalfx/apmworkshop/master/setup-tools/bashrc -o bashrc
+curl https://raw.githubusercontent.com/signalfx/otelworkshop/master/setup-tools/bashrc -o /tmp/bashrc
 echo -e "\n\n" >> /home/ubuntu/.bashrc
-cat bashrc >> /home/ubuntu/.bashrc
-rm bashrc
+cat /tmp/bashrc >> /home/ubuntu/.bashrc
+rm /tmp/bashrc
 source /home/ubuntu/.bashrc
--- setup-tools/k8s-env-only.sh
+++ setup-tools/k8s-env-only.sh
@@ -3,29 +3,25 @@ sudo apt-get -y update
 #install helm
 curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-#install k3s
-curl -sfL https://get.k3s.io | sh -
-
 #install otel
 helm repo add splunk-otel-collector-chart https://signalfx.github.io/splunk-otel-collector-chart
 helm repo update
 
 #enable helm to access cluster
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-sudo chmod 755 /etc/rancher/k3s/k3s.yaml
+mkdir /home/ubuntu/.kube && sudo kubectl config view --raw > /home/ubuntu/.kube/config
 
 #install text browser
 sudo apt install -y lynx
 
+#clone workshop
+git clone https://github.com/signalfx/otelworkshop
+
 #install k9s
 curl -sS https://webinstall.dev/k9s | bash
 
-#clone workshop
-git clone https://github.com/signalfx/apmworkshop
-
 #update .bashrc for workshop
-curl https://raw.githubusercontent.com/signalfx/apmworkshop/master/setup-tools/bashrc -o /tmp/bashrc
+curl https://raw.githubusercontent.com/signalfx/otelworkshop/master/setup-tools/bashrc -o bashrc
 echo -e "\n\n" >> /home/ubuntu/.bashrc
-cat /tmp/bashrc >> /home/ubuntu/.bashrc
-rm /tmp/bashrc
+cat bashrc >> /home/ubuntu/.bashrc
+rm bashrc
 source /home/ubuntu/.bashrc
