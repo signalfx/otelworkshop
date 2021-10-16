669e5ee0df911a9d11739df7799580ca1d88b371
unknown <slerner@splunk.com>
2021-10-16 16:49:31 -0400

    commit

---
+++ misc/dotnet-manual/Program.cs
@@ -0,0 +1,55 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Sample.DistributedTracing
+{
+    class Program
+    {
+        static ActivitySource s_source = new ActivitySource("Sample.DistributedTracing");
+
+        public static async Task Main()
+                {
+                    while(true)
+                    {
+                        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MySample"))
+                            .AddSource("Sample.DistributedTracing")
+                            .AddConsoleExporter()
+                            .AddOtlpExporter()
+                            .Build();
+
+                        await DoSomeWork();
+                        Console.WriteLine("Example work done");
+                    }
+                }
+
+        static async Task DoSomeWork()
+        {
+            using (Activity a = s_source.StartActivity("SomeWork"))
+            {
+                await StepOne();
+                await StepTwo();
+            }
+        }
+
+        static async Task StepOne()
+        {
+            using (Activity a = s_source.StartActivity("StepOne"))
+            {
+                await Task.Delay(250);
+            }
+        }
+
+        static async Task StepTwo()
+        {
+            using (Activity a = s_source.StartActivity("StepTwo"))
+            {
+                await Task.Delay(250);
+            }
+        }
+    }
+}
---
+++ misc/dotnet-manual/index.md
@@ -0,0 +1,11 @@
+This example shows creation of OTLP traces, manually, from a .NET application.
+
+It requires .NET 5.
+
+It is designed to be run on a host with the OpenTelemetry Collector running locally with the standard configuration of port 4317 listening for OTLP traces.
+
+To initiate use:
+
+```bash
+dotnet run
+```
