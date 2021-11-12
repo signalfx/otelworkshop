b238bfa782409ad875a79ecc34ee66bb2229c10a
Steve L <slerner@splunk.com>
2021-11-11 21:49:13 -0500

    Update and rename jaeger.py to jaegerexample.py

--- misc/manual/python/jaegerexample.py
+++ misc/manual/python/jaegerexample.py
@@ -1,5 +1,10 @@
-# tested with:
+# Requires
+# pip3 install opentelemetry-api
+# pip3 install opentelemetry-sdk
+# pip3 install opentelemetry-exporter-jaeger
+# pip3 install keyboard
 
+# tested with:
 # opentelemetry-api                        1.7.1               
 # opentelemetry-exporter-jaeger            1.7.1               
 # opentelemetry-exporter-jaeger-proto-grpc 1.7.1               
@@ -7,18 +12,14 @@
 # opentelemetry-sdk                        1.7.1               
 # opentelemetry-semantic-conventions       0.26b1 
 
-# pip3 install opentelemetry-api
-# pip3 install opentelemetry-sdk
-# pip3 install opentelemetry-exporter-jaeger
-
 
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+# from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+import time
 
 trace.set_tracer_provider(
     TracerProvider(
@@ -36,13 +37,11 @@ jaeger_exporter = JaegerExporter(
 trace.get_tracer_provider().add_span_processor(
     BatchSpanProcessor(jaeger_exporter)
 )
-
-tracer = trace.get_tracer(__name__)
-
-with tracer.start_as_current_span("foo"):
-    with tracer.start_as_current_span("bar"):
-        with tracer.start_as_current_span("baz"):
-            print("Hello world from OpenTelemetry Python!")
-
-
+while(True):
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("foo"):
+        with tracer.start_as_current_span("bar"):
+            with tracer.start_as_current_span("baz"):
+                print("Hello world from OpenTelemetry Python! CTRL-C to quit")
+                time.sleep(.250)
 trace.get_tracer_provider().shutdown()
