14bd7e43673c58d0b366dc63e6692a3e6d6dbb73
slerner@splunk.com <slerner@splunk.com>
2021-11-14 22:29:28 -0500

    commit

--- misc/docker/run-dotnet-autogen-debug.sh
+++ misc/docker/run-dotnet-autogen-debug.sh
@@ -1,5 +1,5 @@
 docker run --rm -dit \
--e SIGNALFX_SERVICE_NAME=dotnet-autogen \
+-e SIGNALFX_SERVICE_NAME=dotnet-autogen-debug \
 -e SIGNALFX_ENDPOINT_URL=otelcol:9411/api/v2/spans \
 -e SIGNALFX_TRACE_GLOBAL_TAGS=deployment.environment:apm-workshop \
 -e CORECLR_ENABLE_PROFILING=1 \
@@ -7,6 +7,9 @@ docker run --rm -dit \
 -e CORECLR_PROFILER_PATH=/opt/signalfx-dotnet-tracing/SignalFx.Tracing.ClrProfiler.Native.so \
 -e SIGNALFX_INTEGRATIONS=/opt/signalfx-dotnet-tracing/integrations.json \
 -e SIGNALFX_DOTNET_TRACER_HOME=/opt/signalfx-dotnet-tracing \
+-e SIGNALFX_TRACE_DEBUG=true \
+-e SIGNALFX_TRACE_LOG_PATH=/opt/tracelogs \
+-e SIGNALFX_STDOUT_LOG_ENABLED=1 \
 --network otel-net \
 --name dotnet-autogen docker.io/stevelsplunk/splk-dotnet5
 
